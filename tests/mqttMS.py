6a7a55a1d3f63586fc1f3e1ba796a06f3bb41016
FilippoOnesti <filippo.onesti@edu.unife.it>
2024-06-11 14:49:15 +0200

    Fixed Python Tests

---
+++ tests/mqttMS.py
@@ -0,0 +1,39 @@
+import paho.mqtt.client as mqtt
+import json
+
+def on_connect(client, userdata, flags, rc):
+  print("Connected with result code "+str(rc))
+  client.subscribe("records")
+
+def on_message(client, userdata, msg):
+  print("Received message on topic '"+msg.topic+"'")
+  if Operation._write_file(msg.payload):
+    print("Record saved")
+  else:
+    print("Error saving record")
+
+class Operation():
+  def _write_file(msg):
+    try:
+      jdata = json.loads(msg)
+      print("Received record with "+str(len(jdata)-1)+" readings named "+jdata[-1]["name"])
+      filename = jdata[-1]["name"]
+      jdata.pop()
+      f = open("testrecords/"+filename, "w")
+      f.write(str(json.dumps(jdata, sort_keys=True, indent=4)))
+      f.close()
+      return True
+    except Exception as e:
+      return False
+
+def main():
+  client = mqtt.Client()
+  client.connect("192.168.1.55",1883,60)
+
+  client.on_connect = on_connect
+  client.on_message = on_message
+
+  client.loop_forever()
+
+if __name__ == "__main__":
+    main()
--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('/home/filippoonesti/mobile_sensing')
+#sys.path.append('/home/filippoonesti/mobile_sensing')
 from mqttMS import Operation
 import pytest
 import paho.mqtt.client as mqtt
@@ -9,9 +9,9 @@ class TestUnit():
         expected = True
         msg = '[{"datetime": "13:31:11","g_value": 43,"lat": "44.883560","long": "11.611293"},{"name": "record_dd-mm-yy_hh_mm.json"}]'
         assert Operation._write_file(msg) == expected
-    
+
     def test_write_file_incorrect(self):
         expected = False
         msg = '[{"datetime": "13:31:11","g_value": 43,"lat": "44.883560","long": "11.611293"},,{"name": "record_dd-mm-yy_hh_mm.json"}]'
         assert Operation._write_file(msg) == expected
-    
+
