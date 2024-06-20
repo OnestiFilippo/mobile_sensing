2721103c9b4cc5d2643b48cfcf1fd468c1af4c9e
FilippoOnesti <filippo.onesti@edu.unife.it>
2024-06-20 12:37:53 +0200

    Updated mqttMS.py

--- mqttMS.py
+++ mqttMS.py
@@ -1,23 +1,20 @@
-"""
-Script to receive records from client with MQTT and save it to a file
-"""
 import json
 import paho.mqtt.client as mqtt
-"""
-Function on_connect to define what to do when client connects to broker
-"""
+
+# Function on_connect to define what to do when client connects to broker
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client.subscribe("records")
-"""
-Function on_message to define what to do when client receives a message
-"""
+
+# Function on_message to define what to do when client receives a message
 def on_message(client, userdata, msg):
     print("Received message on topic '"+msg.topic+"'")
     if Operation._write_file(msg.payload):
         print("Record saved")
     else:
         print("Error saving record")
+
+# Class Operation that contains write_file function to write the received record on json file
 class Operation():
     def _write_file(self, msg):
         try:
@@ -31,6 +28,8 @@ class Operation():
             return True
         except Exception as e:
             return False
+
+# Main function
 def main():
     client = mqtt.Client()
     client.connect("192.168.1.55",1883,60)
