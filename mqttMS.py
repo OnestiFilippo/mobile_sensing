f72c1b7e47e729e18f96509be7a7802de13e6d8f
FilippoOnesti <filippo.onesti@edu.unife.it>
2024-08-19 16:44:13 +0200

    Updated MQTT Python script

--- mqttMS.py
+++ mqttMS.py
@@ -6,28 +6,28 @@ def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client.subscribe("records")
 
+# Function to write received message in a json file
+def write_file(msg):
+    try:
+        jdata = json.loads(msg)
+        print("Received record with "+str(len(jdata)-1)+" readings named "+jdata[-1]["name"])
+        filename = jdata[-1]["name"]
+        jdata.pop()
+        f = open("html/records/"+filename, "w")
+        f.write(str(json.dumps(jdata, sort_keys=True, indent=4)))
+        f.close()
+        return True
+    except Exception as e:
+        return False
+
 # Function on_message to define what to do when client receives a message
 def on_message(client, userdata, msg):
     print("Received message on topic '"+msg.topic+"'")
-    if Operation._write_file(msg.payload):
+    print(msg.payload.decode("utf-8"))
+    if write_file(msg.payload):
         print("Record saved")
     else:
         print("Error saving record")
-
-# Class Operation that contains write_file function to write the received record on json file
-class Operation():
-    def _write_file(self, msg):
-        try:
-            jdata = json.loads(msg)
-            print("Received record with "+str(len(jdata)-1)+" readings named "+jdata[-1]["name"])
-            filename = jdata[-1]["name"]
-            jdata.pop()
-            f = open("html/records/"+filename, "w")
-            f.write(str(json.dumps(jdata, sort_keys=True, indent=4)))
-            f.close()
-            return True
-        except Exception as e:
-            return False
 
 # Main function
 def main():
